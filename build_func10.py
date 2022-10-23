c84944702b7d9bbc694db754bb5eab68de38dbcc
1976alishoh <106156697+iqrobocreator@users.noreply.github.com>
2022-10-23 22:02:53 +0300

    10 done

--- build_func10.py
+++ build_func10.py
@@ -1,4 +1,5 @@
 def main(x,y):
+    result = 3*(y**(1/2)) + x**(2/3)
     """
     Given a arguments called 'x' and 'y' type of int , calculate the value of expression and round the result to 2 decimal places, use the pow function use the round function ,return result:
     Args:
@@ -7,5 +8,6 @@ def main(x,y):
     Returns:
         result : float
     """
-    return 
+    return result
+print(main(8,4))
 
