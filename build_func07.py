cd2150eaf09e3f3ff967bc6d78c92ba5b0036f88
1976alishoh <106156697+iqrobocreator@users.noreply.github.com>
2022-10-23 21:51:58 +0300

    07 done

--- build_func07.py
+++ build_func07.py
@@ -1,4 +1,5 @@
 def main(x,y):
+    result = x**2 + 6*(x**3) +3*x*y
     """
     Given a arguments called 'x' and 'y' type of int , calculate the value of expression and return result:
     Args:
@@ -7,4 +8,5 @@ def main(x,y):
     Returns:
         result : int
     """
-    return 
+    return result
+print(main(5,2))
