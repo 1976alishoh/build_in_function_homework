b4e8ebb8ecc722deeee19cdad97dcfecbed3affb
1976alishoh <106156697+iqrobocreator@users.noreply.github.com>
2022-10-23 21:55:21 +0300

    0 done

--- build_func08.py
+++ build_func08.py
@@ -1,4 +1,5 @@
 def main(x,y):
+    result = 5*(x**2)*(y**3) + x*(y**2)
     """
     Given a arguments called 'x' and 'y' type of int , calculate the value of expression and return result:
     Args:
@@ -7,4 +8,5 @@ def main(x,y):
     Returns:
         result : int
     """
-    return 
+    return result
+print(main(7,1))
